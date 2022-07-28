387b32cc99b72f729ea14d421b648b3f3dafb76b
Kaustubh Bhule <63561860+kaustubhbhule@users.noreply.github.com>
2022-07-28 12:12:33 +0530

    Add files via upload

--- files/read_files.asm
+++ files/read_files.asm
@@ -2,21 +2,22 @@
 
 section .data
 	filename db "myfile.txt",0
-	text db "Here's some text."
+
+section .bss
+	text resb 18
 
 section .text
 	global _start
-    
 _start:
 	mov rax, SYS_OPEN
 	mov rdi, filename
-	mov rsi, O_CREAT+O_WRONLY
+	mov rsi, O_RDONLY
 	mov rdx, 0644o
 	syscall
 	
 	push rax
 	mov rdi, rax
-	mov rax, SYS_WRITE
+	mov rax, SYS_READ
 	mov rsi, text
 	mov rdx, 17
 	syscall
@@ -25,4 +26,5 @@ _start:
 	pop rdi
 	syscall
 
+    print text
 	exit
